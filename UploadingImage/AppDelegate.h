446bf7f0de4a19713e1b8a2ba965cba0f1a3711f
DGC <michaelxuyuqin@163.com>
2014-04-16 09:55:14 +0800

    Initial Commit

---
+++ UploadingImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UploadingImage
+//
+//  Created by DGC on 16/4/14.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
